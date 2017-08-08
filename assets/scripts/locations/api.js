d56bc6f05df1e439897588833c1a6f25555a229d
jltyler <john.lucas.tyler@gmail.com>
2017-08-08 12:50:14 -0400

    Added API calls for adding items

--- assets/scripts/locations/api.js
+++ assets/scripts/locations/api.js
@@ -34,8 +34,56 @@ const addLocation = (name) => {
   })
 }
 
+const addActivity = (name) => {
+  return $.ajax({
+    url: config.apiOrigin + '/activities/' + store.location.id,
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: { activity: {name} }
+  })
+}
+
+const addLandmark = (name) => {
+  return $.ajax({
+    url: config.apiOrigin + '/landmarks/' + store.location.id,
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: { landmark: {name} }
+  })
+}
+
+const addRestaurant = (name) => {
+  return $.ajax({
+    url: config.apiOrigin + '/foods/' + store.location.id,
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: { restaurant: {name} }
+  })
+}
+
+const addComment = (name) => {
+  return $.ajax({
+    url: config.apiOrigin + '/comments/' + store.location.id,
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: { comment: {name} }
+  })
+}
+
 module.exports = {
   getLocations,
   getOneLocation,
-  addLocation
+  addLocation,
+  addActivity,
+  addLandmark,
+  addRestaurant,
+  addComment
 }
