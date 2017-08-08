2d0c772d1e91f62ee2bf09f279e777d0244d2377
jltyler <john.lucas.tyler@gmail.com>
2017-08-08 12:51:02 -0400

    Added events for adding items and hooked them

--- assets/scripts/locations/events.js
+++ assets/scripts/locations/events.js
@@ -3,6 +3,7 @@
 const ui = require('./ui')
 const api = require('./api')
 const store = require('../store')
+const getFormFields = require('../../../lib/get-form-fields')
 
 const onGetLocations = function (event) {
   event.preventDefault()
@@ -26,8 +27,72 @@ const onAddLocation = function (event) {
   }
 }
 
+const onAddActivity = function (event) {
+  console.log('onAddActivity')
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('Data: ', data)
+  if (store.location) {
+    api.addActivity(data.activity.name)
+      .then(ui.addActivitySuccess)
+      .then(() => ui.storeOneActivity(data.activity.name))
+      .catch(ui.failure)
+  } else {
+    console.log('No location is selected!')
+  }
+}
+
+const onAddLandmark = function (event) {
+  console.log('onAddLandmark')
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('Data: ', data)
+  if (store.location) {
+    api.addLandmark(data.landmark.name)
+      .then(ui.addLandmarkSuccess)
+      .then(() => ui.storeOneLandmark(data.landmark.name))
+      .catch(ui.failure)
+  } else {
+    console.log('No location is selected!')
+  }
+}
+
+const onAddRestaurant = function (event) {
+  console.log('onAddRestaurant')
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('Data: ', data)
+  if (store.location) {
+    api.addRestaurant(data.restaurant.name)
+      .then(ui.addRestaurantSuccess)
+      .then(() => ui.storeOneRestaurant(data.restaurant.name))
+      .catch(ui.failure)
+  } else {
+    console.log('No location is selected!')
+  }
+}
+
+const onAddComment = function (event) {
+  console.log('onAddComment')
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log('Data: ', data)
+  if (store.location) {
+    api.addComment(data.comment.name)
+      .then(ui.addCommentSuccess)
+      .then(() => ui.storeOneComment(data.comment.name))
+      .catch(ui.failure)
+  } else {
+    console.log('No location is selected!')
+  }
+}
+
 const addHandlers = () => {
   $('.img-wrap').on('click', onAddLocation)
+  $('#-addactivity-modal-form').on('submit', onAddActivity)
+  $('#-addlandmark-modal-form').on('submit', onAddLandmark)
+  $('#-addrestaurant-modal-form').on('submit', onAddRestaurant)
+  $('#-addcomment-modal-form').on('submit', onAddComment)
 }
 
 module.exports = {
