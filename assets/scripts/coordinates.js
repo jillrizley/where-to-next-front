665e880b7fa2594c898d4fdb259d260dbf43799f
rtuttle611 <rtuttle611@yahoo.com>
2017-08-09 08:59:36 -0400

    started coordinates

---
+++ assets/scripts/coordinates.js
@@ -0,0 +1,131 @@
+'use strict'
+
+mondule.exports=[
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'Honolulu',
+    lat: 21.3069,
+    long: -157.8583
+  },
+
+  {
+    name: 'Madrid',
+    lat: 40.4168,
+    long: -3.7038
+  },
+
+  {
+    name: 'Las Vegas',
+    lat: 36.1699,
+    long: -115.1398
+  },
+
+  {
+    name: 'Berlin',
+    lat: 52.5200,
+    long: 13.4050
+  },
+
+  {
+    name: 'Rio ',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+
+  {
+    name: 'london',
+    lat: 51.5074,
+    long: -0.1278
+  },
+]
+
+
+Rio: 22.9068° S, 43.1729° W
+
+Beijing: 39.9042° N, 116.4074° E
+
+LA: 34.0522° N, 118.2437° W
+
+Cairo: 30.0444° N, 31.2357° E
+
+Sydney: 33.8688° S, 151.2093° E
+
+Moscow: 55.7558° N, 37.6173° E
+Boston: 42.3601° N, 71.0589° W
+Tokyo: 35.6895° N, 139.6917° E
+Austin: 30.2672° N, 97.7431° W
+Milan: 45.4642° N, 9.1900° E
+NYC: 40.7128° N, 74.0059° W
+Paris: 48.8566° N, 2.3522° E
+Montreal: 45.5017° N, 73.5673° W
